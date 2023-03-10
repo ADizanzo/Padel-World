c4aca2d6292e637d694ca7fd6ea7abc5c9d6dc67
Augusto Dizanzo <105518055+ADizanzo@users.noreply.github.com>
2023-03-10 12:39:23 -0300

    Add files via upload

---
+++ js/swetalert.js
@@ -0,0 +1,10 @@
+Swal.fire({
+	title:"Bienvenidos a Padel World",
+	text:"Viví el Padel con Intensidad",
+	timer:2500,
+	timerProgressBar: true,
+	icon:"info",
+	position: "center",
+	showConfirmButton: false,
+
+});
